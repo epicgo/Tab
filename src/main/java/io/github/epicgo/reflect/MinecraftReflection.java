b668c3f2132874848bbabb9d48e9a631a6906d37
Epicgo <105683201+epicgo@users.noreply.github.com>
2024-02-08 02:17:45 -0600

    Se actualizo las clases con referencias a clases de CraftBukkit y Minecraft.

--- src/main/java/io/github/epicgo/reflect/MinecraftReflection.java
+++ src/main/java/io/github/epicgo/reflect/MinecraftReflection.java
@@ -1,5 +1,17 @@
 package io.github.epicgo.reflect;
 
 public class MinecraftReflection {
-    
+
+    // Clase de CraftPlayer de CraftBukkit
+    public static final Class<?> CRAFT_PLAYER_CLASS = Reflection.getCraftBukkitClass("entity.CraftPlayer");
+
+    // Clase de Packet de Minecraft
+    public static final Class<?> NMS_PACKET_CLASS = Reflection.getMinecraftClass("Packet");
+
+    // Clase de EntityPlayer de Minecraft
+    public static final Class<?> NMS_ENTITY_PLAYER_CLASS = Reflection.getMinecraftClass("EntityPlayer");
+
+    // Clase de PlayerConnection de Minecraft
+    public static final Class<?> NMS_PLAYER_CONNECTION_CLASS = Reflection.getMinecraftClass("PlayerConnection");
+
 }
